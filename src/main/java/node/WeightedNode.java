67b31358c5087f5d6215e91c8d669c469a49ceaa
mendansandesh <sandesh.mendan93@gmail.com>
2020-07-10 00:09:34 +0530

    WeightedNode with directed edges

---
+++ src/main/java/node/WeightedNode.java
@@ -0,0 +1,61 @@
+package node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author Sandesh Mendan on 08/07/20
+ * @project algorithms-and-datastructures
+ */
+public class WeightedNode {
+    String name;
+    ArrayList<WeightedNode> neighbors = new ArrayList<>();
+    WeightedNode parent;
+    int distance;
+    HashMap<WeightedNode, Integer> weightMap = new HashMap<>();
+
+    public WeightedNode(String name) {
+        this.name = name;
+        distance = Integer.MAX_VALUE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<WeightedNode> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(ArrayList<WeightedNode> neighbors) {
+        this.neighbors = neighbors;
+    }
+
+    public WeightedNode getParent() {
+        return parent;
+    }
+
+    public void setParent(WeightedNode parent) {
+        this.parent = parent;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public HashMap<WeightedNode, Integer> getWeightMap() {
+        return weightMap;
+    }
+
+    public void setWeightMap(HashMap<WeightedNode, Integer> weightMap) {
+        this.weightMap = weightMap;
+    }
+}
