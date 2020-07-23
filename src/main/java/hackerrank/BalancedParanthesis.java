25879a95eb4eba59a3d5dfe3750a23ac1e4e8b1d
mendansandesh <sandesh.mendan93@gmail.com>
2020-07-23 20:12:13 +0530

    balanced paranthesis check

---
+++ src/main/java/hackerrank/BalancedParanthesis.java
@@ -0,0 +1,42 @@
+package hackerrank;
+
+import java.util.Stack;
+
+public class BalancedParanthesis {
+    public static void main(String[] args){
+        System.out.println(isBalanced("{[(])}"));
+    }
+
+    static String isBalanced(String s) {
+        Stack<Character> stk = new Stack<Character>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '[' || ch == '{' || ch == '(' )
+                stk.push(ch);
+            else if(ch == ']' || ch == '}' || ch == ')'){
+                if(stk.isEmpty())
+                    return "NO";
+                switch(ch){
+                    case ']':
+                        if(stk.pop() != '[')
+                            return "NO";
+                        break;
+                    case '}':
+                        if(stk.pop() != '{')
+                            return "NO";
+                        break;
+                    case ')':
+                        if(stk.pop() != '(')
+                            return "NO";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        if(stk.isEmpty())
+            return "YES";
+        return "NO";
+    }
+}
