5d6cc5df3e59dece02b99cc01ca03eedc4a07f8c
mendansandesh <sandesh.mendan93@gmail.com>
2020-08-17 20:20:43 +0530

    find repeated and missing num in an array

---
+++ src/main/java/problems/geeksforgeeks/string/RepeatedAndMissingNumber.java
@@ -0,0 +1,27 @@
+package problems.geeksforgeeks.string;
+
+/**
+ * @author sandesh.mendan on 14/08/20
+ * @project algorithms-and-datastructures
+ */
+public class RepeatedAndMissingNumber {
+    public static void main(String[] args){
+        int[] arr = {2, 1, 3, 2};
+        findRepeatedAndMissingNumber(arr);
+    }
+
+    private static void findRepeatedAndMissingNumber(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            int absVal = Math.abs(arr[i]);
+            if(arr[absVal - 1] > 0)
+                arr[absVal - 1] = -arr[absVal - 1];
+            else
+                System.out.println("Repeated num is: " + absVal);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > 0)
+                System.out.println("Missing num is: " + (i+1));
+        }
+    }
+}
