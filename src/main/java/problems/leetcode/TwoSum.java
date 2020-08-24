3280e95db4e872b91ac16c62f807788eaadfbb97
mendansandesh <sandesh.mendan93@gmail.com>
2020-08-24 23:40:08 +0530

    return indices of the two numbers such that they add up to a specific target

---
+++ src/main/java/problems/leetcode/TwoSum.java
@@ -0,0 +1,30 @@
+package problems.leetcode;
+
+import problems.utils.Helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sandesh.mendan on 24/08/20
+ * @project algorithms-and-datastructures
+ */
+//Problem: https://leetcode.com/problems/two-sum/
+// TimeComplexity:  O(N)
+// SpaceComplexity: O(N)
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {-1, -2, -3, -4, -5};
+        int[] indices = twoSum(arr, -8);
+        Helper.printArray(indices);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i]))    //if difference exists
+                return new int[]{map.get(nums[i]), i};
+            map.put(target - nums[i], i); //store the difference -> index
+        }
+        return new int[]{-1, -1};
+    }
+}
