9cba66ce313550da116b1ed1c8a05b73d3fcc4d0
mendansandesh <sandesh.mendan93@gmail.com>
2020-10-05 20:40:29 +0530

    Level: Medium - find the minimum cost to reach end of the cell in a 2D array

--- src/main/java/problems/dynamicprogramming/MinCostToReachLastCell.java
+++ src/main/java/problems/dynamicprogramming/MinCostToReachLastCell.java
@@ -20,7 +20,8 @@ public class MinCostToReachLastCell {
                         { 7, 1, 7, 3, 7 },
                         { 2, 9, 8, 9, 3 }
                 };
-        System.out.println(findMinCostTopDown(array, array.length-1, array[0].length-1));
+        //System.out.println(findMinCostTopDown(array, array.length-1, array[0].length-1));
+        System.out.println(findMinCostBottomUp(array, array.length-1, array[0].length-1));
     }
     private static int findMinCostTopDown(int[][] cost, int row, int col) {
         int[][] res = new int[row+1][col+1];
@@ -42,15 +43,16 @@ public class MinCostToReachLastCell {
 
     private static int findMinCostBottomUp(int[][] cost, int row, int col) {
         int[][] dp = new int[cost.length+1][cost[0].length+1];
-        for(int i = 0; i <= dp.length; i++) {  //Initialize default value
+        for(int i = 0; i < dp.length; i++)
             dp[0][i] = Integer.MAX_VALUE;
-        }
-        for(int i = 0; i <= row; i++) {  //Initialize default value
-            dp[i][0] = Integer.MAX_VALUE;
-        }
 
-        for(int i = 1; i <= cost.length; i++){
-            for(int j = 1; j <= cost[0].length; j++){
+        for(int i = 0; i < dp[0].length; i++)
+            dp[i][0] = Integer.MAX_VALUE;
+
+        dp[0][1] = 0; //This is done to make sure that first comparison goes through successfully
+
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
                 dp[i][j] = cost[i-1][j-1] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
