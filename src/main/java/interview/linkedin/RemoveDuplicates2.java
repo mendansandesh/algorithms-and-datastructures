78571b5206e45184fb82317ccde56ae804656cfa
mendansandesh <sandesh.mendan93@gmail.com>
2020-11-30 15:20:10 +0530

    Remove all adjacent duplicates of size k in a string

--- src/main/java/interview/linkedin/RemoveDuplicates2.java
+++ src/main/java/interview/linkedin/RemoveDuplicates2.java
@@ -9,18 +9,46 @@ package interview.linkedin;
 // Space: O(N)
 public class RemoveDuplicates2 {
     public static void main(String[] a){
-        String s = "deeedbbcccbdaa";
+        String s = "deeedbbcccbdppaa";
         int k = 3;
         System.out.println(removeDuplicates(s, k));
     }
 
     public static String removeDuplicates(String s, int k) {
-        StringBuilder sb = new StringBuilder(s); //to add/delete char from string
-        int[] freqCount = new int[sb.length()]; //to track count of corresponding char from sb
-        int index = 0; //
+        char[] chars = new char[s.length()];//to add/delete char from string
+        int[] freqCount = new int[chars.length]; //to track count of corresponding char from sb
+        int i = 0;//index to track chars array
+        for(int j = 0; j < s.length(); j++){
+            char currChar = s.charAt(j);
+            if(i > 0 && chars[i-1] == currChar){
+                chars[i] = currChar;
+                freqCount[i] = freqCount[i-1] + 1;
+                if(freqCount[i] == k) // when freqCount reaches threshold k; remove that char by decrementing i by k
+                    i = i - k;
+            }else {
+                chars[i] = currChar;
+                freqCount[i] = 1;
+            }
+            i++;
+        }
+        return new String(chars, 0, i);
 
-        while (index < sb.length()){
-                if(index == 0 || sb.charAt(index) != sb.charAt(index - 1)) {
+        //below also same logic; coded slightly differently
+        /*StringBuilder sb = new StringBuilder(s); //to add/delete char from string
+        int[] freqCount = new int[sb.length()]; //to track count of corresponding char from sb
+        int i = 0; //index to track sb
+
+        while (i < sb.length()){
+                if(i > 0 && sb.charAt(i) == sb.charAt(i - 1)){
+                    freqCount[i] = freqCount[i - 1] + 1;
+                    if(freqCount[i] == k){
+                        sb.delete(i - k + 1, i + 1);
+                        i = i - k;
+                    }
+                }else {
+                    freqCount[i] = 1;
+                }
+                *//*if(index == 0 || sb.charAt(index) != sb.charAt(index - 1)) {
                     freqCount[index] = 1;
                 }
                 else{
@@ -29,10 +57,10 @@ public class RemoveDuplicates2 {
                         sb.delete(index - k + 1, index + 1); //+1 because index starting from 0
                         index = index - k;
                     }
-                }
-                index++;
+                }*//*
+                i++;
         }
 
-        return sb.toString();
+        return sb.toString();*/
     }
 }
