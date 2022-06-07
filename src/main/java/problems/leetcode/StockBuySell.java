8f274edba3d12f1492e8e24fc2fe78c62ca2572e
sandesh.mendan <sandesh.mendan@algonomy.com>
2022-06-07 22:39:45 +0530

    #easy #blind75 | T O(N) | S O(1)

--- src/main/java/problems/leetcode/StockBuySell.java
+++ src/main/java/problems/leetcode/StockBuySell.java
@@ -5,8 +5,6 @@ package problems.leetcode;
  * @project algorithms-and-datastructures
  */
 // Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-// TimeComplexity:  O(N)
-// SpaceComplexity: O(1)
 public class StockBuySell {
     public static void main(String[] args){
         int[] arr = {2,4,1};
